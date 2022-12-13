1ba4d34c62c81723007147c7da56a9cc5b80c4c8
Altynbek <mr.aseev14@gmail.com>
2022-12-13 13:18:19 +0600

    advice of Rafada

--- handlers/users/chart_of_accounts/common_knowledge.py
+++ handlers/users/chart_of_accounts/common_knowledge.py
@@ -5,6 +5,7 @@ Part of the code responsible for preparing options and compiling tests
 from .game_config import chart_all_values, wiring_all_values
 import random
 from keyboards.inline.ikrandom import Knowledge_keyboard
+from Data.data import praises
 
 
 class Common_CallBack_Handler:
@@ -27,9 +28,10 @@ class Common_CallBack_Handler:
         values = random.choices(list(all_list), k=4)
         true_key = random.choice(values)
         ikb = Knowledge_keyboard(values=values, g=2, true_value=true_key)
+        praise = random.choice(praises)
 
         self.start_text = f"Для начала начнем со следущего счета:\n{all_list.get(true_key).strip()}"
-        self.right_text = f"✅ Молодец!!! Все верно\n{self.next_text}{all_list.get(true_key).strip()}"
+        self.right_text = f"✅ {praise} Все верно\n{self.next_text}{all_list.get(true_key).strip()}"
         self.wrong_text = f"❌ Ответ был неверным\n{self.next_text}{all_list.get(true_key).strip()}"
 
         ikb.ikb('chart')
@@ -52,10 +54,11 @@ a)\n{ikb.a[0]}\n\
 b)\n{ikb.b[0]}\n\
 c)\n{ikb.c[0]}\n\
 d)\n{ikb.d[0]}'
+        praise = random.choice(praises)
 
         self.start_text = f"Для начала начнем со следущей проводки:\n\
         {all_list.get(true_key).strip()}\n{answer_options}"
-        self.right_text = f"✅ Молодец!!! Все верно\n{self.next_text}{all_list.get(true_key).strip()}\n{answer_options}"
+        self.right_text = f"✅ {praise} Все верно\n{self.next_text}{all_list.get(true_key).strip()}\n{answer_options}"
         self.wrong_text = f"❌ Ответ был неверным\n{self.next_text}{all_list.get(true_key).strip()}\n{answer_options}"
 
         ikb.ikb('wiring')
